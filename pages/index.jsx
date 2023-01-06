ae1c92f4c83df6495169b6e6704794ea7fc29c08
ConnorG255 <25connorgriffin@gmail.com>
2023-01-06 08:54:42 -0800

    format changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ import { Inter } from '@next/font/google'
 import { useEffect, useState } from 'react'
 import { marked } from 'marked';
 
+
 /* Use google drive api, work out account thing later, 
 upload this jan 1, make a logo add day 2, try to add api day 3
 and try to deploy on day 4 */
@@ -18,6 +19,12 @@ export default function Home() {
   const [edit,setEdit] = useState(startingstate);
   const [mark, setMarked] = useState("");
 
+ /* function copy(){
+    
+      var Text = document.getElementById("textbox");
+      Text.select();
+      document.execCommand("copy");
+  } */
    useEffect(
     () => {
       setMarked(marked(edit));
@@ -30,9 +37,12 @@ export default function Home() {
     <>
     <title> Luna - Markdown Editor</title>
     <div className='w-95% h-screen bg-slate-900 text-amber-200'>
-      <div className='h-20 w-full flex justify-start mt-0 bg-indigo-900'>
-        <h1>🌙 Luna</h1>
-        
+      <div className='h-20 w-full flex mt-0 bg-indigo-900'>
+       <div className='flex justify-start h-max w-1/2 '><h1>🌙 Luna</h1></div> 
+       
+        <div className=' h-full w-1/2 justify-center text-center'>
+          <button onClick={() => {navigator.clipboard.writeText(edit)}} id="button" className='text-xl font-semibold mt-2 ml-6 w-60 h-14 rounded-lg hover:opacity-95 bg-slate-800'>Copy</button>
+          </div>
       </div>
 
       <div className='flex pt-5 w-99% h-full'>
